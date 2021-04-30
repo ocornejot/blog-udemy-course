78393b9e5b19e49f21ce365cdf7c935a3982f3bf
Octavio Cornejo <ocornejot@gmail.com>
2021-04-30 12:56:30 -0500

    Users sign up implemented

---
+++ spec/features/users/singing_up_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Singup users' do
+  scenario 'with valid credentials' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "secret"
+    fill_in "Password confirmation", with: "secret"
+    click_button 'Sign up'
+
+    expect(page).to have_content('You have signed up successfully')
+  end
+
+  scenario 'with invalid credentials' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button 'Sign up'
+
+    expect(page).to have_content('prohibited this user from being saved')
+  end
+end
