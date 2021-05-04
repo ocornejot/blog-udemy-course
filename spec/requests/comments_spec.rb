a42a33b6121b0f8ec3f97f41c36817fca270f4ef
Octavio Cornejo <ocornejot@gmail.com>
2021-05-04 09:57:54 -0500

    Add user restrictions to create comments

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = User.create!(email: 'user@mail.com', password: 'secret')
+    @user2 = User.create!(email: 'user2@mail.com', password: 'secret')
+    @article = Article.create!(title: 'title', body: 'body of the article.', user: @user)
+  end
+
+  describe 'POST /articles/:id/comments' do
+    context 'with a non signed-in user' do
+      before do
+        post "/articles/#{@article.id}/comments", params: { comment: { body: "Awesome blog" } }
+      end
+      
+      it 'redirect user to the signin page' do
+        flash_message = "Please sign in or sign up first"
+        expect(response).to redirect_to(new_user_session_path)
+        expect(response.status).to eq 302
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with a logged in user' do
+      before do
+        login_as(@user2)
+        post "/articles/#{@article.id}/comments", params: { comment: { body: "Awesome blog" } }
+      end
+
+      it 'create the comment successfully' do
+        flash_message = "Comment has been created"
+        expect(response).to redirect_to(article_path(@article.id))
+        expect(response.status).to eq 302
+        expect(flash[:notice]).to eq flash_message
+      end
+    end
   end
 end
